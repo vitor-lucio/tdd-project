ac7ae248e2a42e1b6afe8be67a525e7ee8b5b826
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2022-05-22 15:10:03 -0300

    Iniciando com Testes Funcionais

---
+++ tdd-project/virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\trabalhos_ufmg\tdd-project\tdd-project\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
