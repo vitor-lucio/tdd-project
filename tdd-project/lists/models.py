bf8901315c7f488206399e6bb8a4d0d1a0c4c3b6
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2022-05-22 19:54:51 -0300

    Implementando o Novo Projeto: Ajustando o Modelo

--- tdd-project/lists/models.py
+++ tdd-project/lists/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 class Item(models.Model):
     text = models.TextField(default='')
+
+class List(models.Model):
+    pass
+
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
